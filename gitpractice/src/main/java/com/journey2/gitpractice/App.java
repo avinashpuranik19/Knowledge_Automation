ca2b07c60acb37e73daebc3400a1787dd82c9ea5
avinashpuranik19 <111avinash@gmail.com>
2026-01-27 00:34:54 +0530

    Added print statement to App.java

--- gitpractice/src/main/java/com/journey2/gitpractice/App.java
+++ gitpractice/src/main/java/com/journey2/gitpractice/App.java
@@ -3,5 +3,7 @@ package com.journey2.gitpractice;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Changes made on Github");
+    
   }
 }
